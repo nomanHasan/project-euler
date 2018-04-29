c48eb89d1b8eab13705d804ea6d44ef11059dbb7
Noman Hasan <nomanbinhussein@gmail.com>
2018-04-30 00:24:21 +0600

    Problem 13 Done

---
+++ dotnet/12.high-div-triang-num/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.high_div_triang_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int i = 1;
+            int n = 2;
+
+            while (true)
+            {
+                i = Enumerable.Range(1, n).Aggregate((accm, obj) => accm + obj);
+                n++;
+
+                int divisorCount = divisors(i).Length;
+                Console.WriteLine(divisorCount);
+
+                if (divisorCount > 500)
+                {
+                    Console.WriteLine("Number is : " + i + ". Divisor Count: " + divisorCount);
+                    break;
+                }
+            }
+
+
+
+        }
+
+        // Answer: 76576500.
+
+        static int[] divisors(int number)
+        {
+            return Enumerable.Range(1, number).Where(x => number % x == 0).ToArray();
+        }
+
+
+    }
+}
